d478e6ad21910a7b679c3cd605dbe720846c6bf9
theomgxz <sg28r8@gmail.com>
2019-10-13 16:15:49 -0400

    Added type function

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,17 +1,33 @@
 use std::collections::VecDeque;
-// use regex::Regex;
+use regex::Regex;
 use std::string::String;
-// pub enum Type{
-    
-//     Digits,
-//     Identifiers,
-    
-// }
-// impl Type{
-//     fn match_token(&self, string_token: &str){       
-        
-//     }
-// }
+pub enum Type{    
+/*
+ *The type of token to return to interpreter 
+ */
+    Digits,
+    Identifiers,
+    Plus,
+    Minus,
+    Divide,
+    LeftParenthesis,
+    RightParenthesis,
+    whoknows
+}
+impl Type{
+    //this might be a little redundant, but ehh
+    pub fn match_type(a_type:Type)->String{
+        match a_type{
+            Type::Plus=> String::from("Plus"),
+            Type::Minus=> String::from("Minus"),           
+            Type::LeftParenthesis=> String::from("Parenthesis"),
+            Type::RightParenthesis=> String::from("Parenthesis"),
+            Type::Divide => String::from("Divide"),
+            _ => String::from("who_knows")
+        }
+    }
+}
+
 pub struct Lexer<'a>{
     pub a_value: &'a String
 }
@@ -43,11 +59,35 @@ impl<'a> Lexer<'a>{
         }         
         token_queue
     }
-    pub fn token_return(&self, mut token_q : VecDeque<String>)->Option< String>{
-        while !token_q.is_empty(){
+    pub fn return_type(&self,some_string:String)->Option<Type>{
+        let digit_regex = Regex::new(r"\d+").unwrap();
+        // let is_identifier =Regex::new(r"");
+        match some_string.as_str(){
+            "+" => Some(Type::Plus),
+            "-"=> Some(Type::Minus),            
+            "("=> Some(Type::LeftParenthesis),
+            ")"=> Some(Type::RightParenthesis),
+            "/" => Some(Type::Divide),
+            _ => Some(Type::whoknows)
+        };
+        if digit_regex.is_match(some_string.as_str()){
+            return Some(Type::Digits);
+        }
+        else{
+            return None
+        }
+
+        
+    }   
+    pub fn token_return(&self, mut token_q : VecDeque<String>)->Option<String>{
+        if !token_q.is_empty(){
             let b = token_q.pop_front();
             return b;
         }
-        return None;        
-    }   
+        else{
+            return None;
+
+        } 
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ mod lexer;
 use std::string::String;
 use std::collections::VecDeque;
 fn main() {
-    let _some_string : String = "a+b".to_string();
+    let _some_string : String = "+".to_string();
     let _a_token = lexer::Lexer{a_value:&_some_string};
+    
     let token_vector: VecDeque<String> = _a_token.get_next_token();  
     println!("{:?}",token_vector);
-    println!("{:?}",_a_token.token_return(token_vector).unwrap());
+    // println!("{:?}",_a_token.token_return(token_vector).unwrap());
+    let c = lexer::Type::match_type(_a_token.return_type(_a_token.token_return(token_vector).unwrap()).unwrap());
+
+    println!("{:}",c);
 
 }
