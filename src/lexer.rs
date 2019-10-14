87526b0e5e7063b7e9d09ff19e5d0148b89cd9a6
theomgxz <sg28r8@gmail.com>
2019-10-14 16:02:48 -0400

    fixed some issues

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,93 +1,103 @@
-use std::collections::VecDeque;
 use regex::Regex;
+use std::collections::VecDeque;
 use std::string::String;
-pub enum Type{    
-/*
- *The type of token to return to interpreter 
- */
+#[derive(Debug)]
+pub enum Type {
+    /*
+     *The type of token to return to interpreter
+     */
     Digits,
-    Identifiers,
+    // Identifiers,
     Plus,
     Minus,
     Divide,
     LeftParenthesis,
     RightParenthesis,
-    whoknows
+    // EndOfLine,
+    SemiColon,
+    NewLine,
+    Whoknows,
 }
-impl Type{
+
+impl Type {
     //this might be a little redundant, but ehh
-    pub fn match_type(a_type:Type)->String{
-        match a_type{
-            Type::Plus=> String::from("Plus"),
-            Type::Minus=> String::from("Minus"),           
-            Type::LeftParenthesis=> String::from("Parenthesis"),
-            Type::RightParenthesis=> String::from("Parenthesis"),
-            Type::Divide => String::from("Divide"),
-            _ => String::from("who_knows")
+    pub fn match_type(a_type: Option<Type>) -> String {
+        match a_type {
+            Some(Type::Plus) => String::from("Plus"),
+            Some(Type::Minus) => String::from("Minus"),
+            Some(Type::LeftParenthesis) => String::from("Parenthesis"),
+            Some(Type::RightParenthesis) => String::from("Parenthesis"),
+            Some(Type::Divide) => String::from("Divide"),
+            Some(Type::Digits) => String::from("Digits"),
+            Some(Type::Whoknows) => String::from("who_knows"),
+            Some(Type::SemiColon) => String::from("Semi colon"),
+            Some(Type::NewLine) => String::from("newline"),
+            // Some(Type::EndOfLine) => String::from("EOL"),
+            None => String::from("ther is nothing bro"),
         }
     }
 }
 
-pub struct Lexer<'a>{
-    pub a_value: &'a String
+pub struct Lexer<'a> {
+    pub a_value: &'a String,
 }
-impl<'a> Lexer<'a>{    
-    pub fn get_next_token(&self)->VecDeque<String>{
+
+impl<'a> Lexer<'a> {
+    pub fn get_next_token(&self) -> VecDeque<String> {
         /*
          *The follwing function takes in string tokenizes them(seperating by whitespace) and returns token in fifo fashion
          */
-        let string_to_break =  self.a_value.split_whitespace();
-        let mut token_queue:VecDeque<String> = VecDeque::new();
-        for token in string_to_break{
-            let mut char_vec:Vec<char> = token.chars().collect();
-            let rem_V = char_vec.clone();
-            let expression_list = ["{","}","+","=","-","/","<",">","(",")"];
-            
-            for (i,chara) in rem_V.iter().enumerate(){
+        let string_to_break = self.a_value.split_whitespace();
+        let mut token_queue: VecDeque<String> = VecDeque::new();
+        for token in string_to_break {
+            let mut char_vec: Vec<char> = token.chars().collect();
+            let rem_v = char_vec.clone();
+            let expression_list = ["{", "}", "+", "=", "-", "/", "<", ">", "(", ")"];
 
+            for (i, chara) in rem_v.iter().enumerate() {
                 let ign = chara.to_string();
-                println!("{:}",ign);
-                for item in expression_list.iter(){
-                    if &ign == item{                        
+                println!("{:}", ign);
+                for item in expression_list.iter() {
+                    if &ign == item {
                         token_queue.push_back(ign.clone());
                         char_vec.remove(i);
                     }
                 }
             }
-            let token_now : String = char_vec.iter().collect();
+            let token_now: String = char_vec.iter().collect();
             token_queue.push_back(token_now);
-        }         
+        }
         token_queue
     }
-    pub fn return_type(&self,some_string:String)->Option<Type>{
-        let digit_regex = Regex::new(r"\d+").unwrap();
-        // let is_identifier =Regex::new(r"");
-        match some_string.as_str(){
-            "+" => Some(Type::Plus),
-            "-"=> Some(Type::Minus),            
-            "("=> Some(Type::LeftParenthesis),
-            ")"=> Some(Type::RightParenthesis),
-            "/" => Some(Type::Divide),
-            _ => Some(Type::whoknows)
-        };
-        if digit_regex.is_match(some_string.as_str()){
+    pub fn matches_digit_and_id( a_string:&str)->Option<Type>{
+        let digit_regex = Regex::new(r"\d").unwrap();
+        if digit_regex.is_match(a_string){
             return Some(Type::Digits);
-        }
-        else{
-            return None
-        }
-
-        
-    }   
-    pub fn token_return(&self, mut token_q : VecDeque<String>)->Option<String>{
-        if !token_q.is_empty(){
-            let b = token_q.pop_front();
-            return b;
-        }
-        else{
+        }else{
             return None;
-
-        } 
+        }
+    }
+    pub fn return_type(&self, some_string: String) -> Option<Type> {
+        println!("{:?}", some_string.as_str());
+        // let a = Regex::new(r"\d").unwrap().is_match(some_string.as_str());
+        match some_string.as_str() {
+            "+" => Some(Type::Plus),
+            "-" => Some(Type::Minus),
+            "(" => Some(Type::LeftParenthesis),
+            ")" => Some(Type::RightParenthesis),
+            "/" => Some(Type::Divide),
+            r"\n" => Some(Type::NewLine),
+            ";" => Some(Type::SemiColon),
+            _ => Lexer::matches_digit_and_id(some_string.as_str()),       
+        }
     }
 
+    pub fn token_return(&self, mut token_q: VecDeque<String>) -> Option<String> {
+        if !token_q.is_empty() {
+            let b = token_q.pop_front();
+            return b;
+        } else {
+            return None;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ mod lexer;
 use std::string::String;
 use std::collections::VecDeque;
 fn main() {
-    let _some_string : String = "+".to_string();
+    let _some_string = String::from("2a");
     let _a_token = lexer::Lexer{a_value:&_some_string};
     
     let token_vector: VecDeque<String> = _a_token.get_next_token();  
     println!("{:?}",token_vector);
+    let token_now_i = _a_token.token_return(token_vector).unwrap();
     // println!("{:?}",_a_token.token_return(token_vector).unwrap());
-    let c = lexer::Type::match_type(_a_token.return_type(_a_token.token_return(token_vector).unwrap()).unwrap());
+    let c = _a_token.return_type(token_now_i);
 
-    println!("{:}",c);
+    println!("{:?}",lexer::Type::match_type(c));
 
 }
