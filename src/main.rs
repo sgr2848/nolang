edc82a0014ceff592de656bd1d61bcda4cca29f0
theomgxz <sg28r8@gmail.com>
2019-10-16 12:14:18 -0400

    more change to mains

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod lexer;
 use std::string::String;
 use std::collections::VecDeque;
+// use std::io::{stdin,stdout,Write};
+/**
+ * here we make the basic 
+ */
 fn main() {
-    let _some_string = String::from("a_2 = 1
-    b = 3");
+    let _some_string = String::from("hello = a");
     let _a_token = lexer::Lexer{a_value:&_some_string};
     
     let mut token_vector: VecDeque<String> = _a_token.get_next_token();
     loop{
         println!("{:?}",token_vector);
-        let mut token_now_i = _a_token.token_return(&mut token_vector).unwrap();
+        let token_now_i = _a_token.token_return(&mut token_vector).unwrap();
         // println!("{:?}",_a_token.token_return(token_vector).unwrap());
-        let mut c = _a_token.return_type(token_now_i);
+        let c = _a_token.return_type(token_now_i);
         println!("{:?}",lexer::Type::match_type(c));   
         if token_vector.is_empty(){
             break;
